8b43abee89dc4598096407aa26f1936a5250b0d6
Serhiy Paluba <palyba8@gmail.com>
2016-06-24 12:33:30 +0300

    changed homework

--- js-01-02/01/js/script.js
+++ js-01-02/01/js/script.js
@@ -5,7 +5,11 @@ number = prompt('Введите число = ', '');
 power = prompt('Введите степень = ', '');
 
 function pow (number,power) {
-return Math.pow(number,power);
+var result=1;
+for(var i=0; i<power;i++){
+	result*=number;
+}
+return result
 }
 
 console.log('Число '+number+' в степени '+power+' равно =',pow(number,power));
