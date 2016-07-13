2bbfb0b52272edd0739a8c0e5854cccdb542ece7
Serhiy Paluba <palyba8@gmail.com>
2016-07-13 17:45:22 +0300

    modif js 03-04

--- js-03-04/js/script.js
+++ js-03-04/js/script.js
@@ -7,29 +7,23 @@ header.innerHTML = 'Тест по программированию';
 document.body.appendChild(header);
 
 //create form element
+var row = document.createElement('div');
+row.className = 'row';
+document.body.appendChild(row);
 var form = document.createElement('form');
-document.body.appendChild(form);
+row.appendChild(form);
 
 //create new Question objects
-var q1 = new Question('1. Вопрос №1?');
-q1.label();
-q1.ansver('Вариант ответа №1');
-q1.ansver('Вариант ответа №2');
-q1.ansver('Вариант ответа №3');
+var q;
+for (var i=1; i<8; i++) {
+	q = new Question(i+'. Вопрос №'+i+'?');
+	q.label();
+	q.ansver('Вариант ответа №1');
+	q.ansver('Вариант ответа №2');
+	q.ansver('Вариант ответа №3');
+};
 
-var q2 = new Question('2. Вопрос №2?');
-q2.label();
-q2.ansver('Вариант ответа №1');
-q2.ansver('Вариант ответа №2');
-q2.ansver('Вариант ответа №3');
 
-var q3 = new Question('3. Вопрос №3?');
-q3.label();
-q3.ansver('Вариант ответа №1');
-q3.ansver('Вариант ответа №2');
-q3.ansver('Вариант ответа №3');
-
-//button
 var button = document.createElement('button');
 button.type = 'submit';
 button.className = 'btn btn-primary center-block';
